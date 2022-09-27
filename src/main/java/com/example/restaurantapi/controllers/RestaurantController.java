ecb8f7ccb464542a92e798ab34a93ae7aebb4276
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-09-27 10:46:13 +0100

    added restaurant controller methods

--- src/main/java/com/example/restaurantapi/controllers/RestaurantController.java
+++ src/main/java/com/example/restaurantapi/controllers/RestaurantController.java
@@ -1,9 +1,15 @@
 package com.example.restaurantapi.controllers;
 
+import com.example.restaurantapi.models.Bill;
 import com.example.restaurantapi.services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/restaurant")
@@ -11,4 +17,14 @@ public class RestaurantController {
 
     @Autowired
     RestaurantService restaurantService;
+
+    @GetMapping(value = "/till")
+    public ResponseEntity<Double> getTillAmount() {
+        return new ResponseEntity<>(restaurantService.getTillAmount(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/closed-bills")
+    public ResponseEntity<List<Bill>> getClosedBills() {
+        return new ResponseEntity<>(restaurantService.getClosedBills(), HttpStatus.OK);
+    }
 }
